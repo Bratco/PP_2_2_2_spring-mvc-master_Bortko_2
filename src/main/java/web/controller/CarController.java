eb46654b30198924364724b5e3229774b2161a93
Aleksandr <bratco@bk.ru>
2026-05-31 11:26:02 +0500

    Добавлен интерфейс

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,10 +1,10 @@
 package web.controller;
 import org.springframework.stereotype.Controller;
-import web.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 
 @Controller
 public class CarController {
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,32 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+    private final List<Car> cars = new ArrayList<>();
+
+    public CarServiceImpl() {
+        cars.add(new Car("Lada", "Priora", 2010));
+        cars.add(new Car("Audi", "80", 1987));
+        cars.add(new Car("Mersedes", "E", 2008));
+        cars.add(new Car("Subaru", "Impreza", 2008));
+        cars.add(new Car("Land Rover", "Discovery", 2011));
+        cars.add(new Car("LandR", "Di", 2013));
+    }
+
+    public List<Car> getCars(Integer count) {
+        if (count == null || count >=cars.size()) {
+            return new ArrayList<>(cars);
+        }
+
+        if (count <= 0) {
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(cars.subList(0, count));
+    }
+}
